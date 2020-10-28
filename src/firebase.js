d8025f880c4221722a38c22215b81182a8bf43a5
acer <rverdufiguera@gmail.com>
2020-10-28 06:47:50 -0300

    listo

--- src/firebase.js
+++ src/firebase.js
@@ -1,134 +1,159 @@
 //Registrandose
-export const registrarseAutenticarse= (email, password)=>{
-    auth
+export const registrarseAutenticarse = (email, password) => {
+  auth
     .createUserWithEmailAndPassword(email, password)
-    .then( userCredential =>{
-        if (userCredential) {
-            window.location.hash = '#/';
-            console.log('hola')
-            // ...
-          } else {
-            window.location.hash = '#/';
-            // User is signed out.
-            // ...
-          }
-        // return response.send(responseString);
-            console.log(userCredential);
-                //User is signed in.
-    }) .catch(error => { alert("Error Usuario No Registrado")})
- 
-
-}
-//Ingresar sesion con cuenta ya registrada
-
-export const iniciarSesionUsuarioPass = (emailRegistrado,passwordRegistrado)=> {
-auth
-.signInWithEmailAndPassword(emailRegistrado, passwordRegistrado) 
-.then ( userCredential => {
-    
-    if (userCredential) {
-        window.location.hash ='#/inicio';
-        console.log('hola')
+    .then((userCredential) => {
+      if (userCredential) {
+        window.location.hash = "#/";
+        // console.log('hola')
         // ...
       } else {
-        window.location.hash ='#/';
+        window.location.hash = "#/";
         // User is signed out.
         // ...
       }
-    // return response.send(responseString);
-        console.log(userCredential);
-            //User is signed in.
-}) .catch(error =>  { console.log("Error")})
-  
+      // return response.send(responseString);
+      // console.log(userCredential);
+      //User is signed in.
+    })
+    .catch((error) => {
+      alert("Error Usuario No Registrado");
+    });
+};
+//Ingresar sesion con cuenta ya registrada
 
-}
+export const iniciarSesionUsuarioPass = (
+  emailRegistrado,
+  passwordRegistrado
+) => {
+  auth
+    .signInWithEmailAndPassword(emailRegistrado, passwordRegistrado)
+    .then((userCredential) => {
+      if (userCredential) {
+        window.location.hash = "#/inicio";
+        // console.log('hola')
+        // ...
+      } else {
+        window.location.hash = "#/";
+        // User is signed out.
+        // ...
+      }
+      // return response.send(responseString);
+      console.log(userCredential);
+      //User is signed in.
+    })
+    .catch((error) => {
+      console.log("Error");
+    });
+};
 // funcion para ingresar por medio de cuentas google
 export const iniciarSesionGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
 
-    window.location.hash='#/inicio'
-
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-}
+      window.location.hash = "#/inicio";
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+};
 
 //funcion para cerrar sesion
 
-export  const cerrarSesionApp = () =>{
- auth.signOut().then(()=>{
-   console.log('salida');
-   window.location.hash='#/inicio'
- })
-}
+export const cerrarSesionApp = () => {
+  auth.signOut().then(() => {
+    //  console.log('salida');
+    window.location.hash = "#/inicio";
+  });
+};
 
 // funcion para recuperar contraseña
 
-export const recuperarContraseña = (emailAddress)=> {
-  auth.sendPasswordResetEmail(emailAddress).then(function() {
-    window.location.hash = '#/'
-    // Email sent.
-  }).catch(function(error) {
-    // An error happened.
-  });
-}
+export const recuperarContraseña = (emailAddress) => {
+  auth
+    .sendPasswordResetEmail(emailAddress)
+    .then(function () {
+      window.location.hash = "#/";
+      // Email sent.
+    })
+    .catch(function (error) {
+      // An error happened.
+    });
+};
 
-// Funcion para las Publicaciones 
+// Funcion para las Publicaciones
 
-export const posteame = (post)=>{
+export const posteame = (post) => {
   const db = firebase.firestore();
 
-  const usuario = () => firebase.auth()
-  .currentUser;
- 
-db.collection("publicaciones").add(post)
-.then(function(docRef) {
-  console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-  console.error("Error adding document: ", error);
-});
+  const usuario = () => firebase.auth().currentUser;
+
+  db.collection("publicaciones")
+    .add(post)
+    .then(function (docRef) {
+      // console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function (error) {
+      // console.error("Error adding document: ", error);
+    });
+};
+//funcion leeme
+export const leeme = () => {
+  console.log("hola leeme");
+  const db = firebase.firestore();
+  db.collection("publicaciones").onSnapshot((querySnapshot) => {
+    // constante vista general de los post
+    const posts = document.getElementById("vistaPost");
+    posts.innerHTML = " ";
+    querySnapshot.forEach((doc) => {
+      const dataPosts = doc.data();
+      const templatePost = `<div><h3>${dataPosts.titulo}</h3> </div>
+        <div>${dataPosts.descripcion}</div>
+        <button class='eliminar' data-id='${doc.id}'>Elimar</button>
+        <button class='editar' data-id='${doc.id}'>Editar</button>
+        `;
+      posts.innerHTML += templatePost; 
+    });
+    const botonesEliminar = document.querySelectorAll('.eliminar');
+    botonesEliminar.forEach((btnEliminar)=>{
+      btnEliminar.addEventListener('click', (event)=>{
+        // console.log(event.target.dataset.id);
+        eliminar(event.target.dataset.id);
+    });
+  });
+    const botonesEditar = document.querySelectorAll('.editar');
+    botonesEditar.forEach((btnEditar)=>{
+      btnEditar.addEventListener('click', ( event)=>{
+        console.log(event.target.dataset.id);
+        editar(event.target.dataset.id);
+      
+    });
+
+    });
+  
+  });
+
+};
+
+export const eliminar = (id) => {
+  const db = firebase.firestore();
+  db.collection("publicaciones").doc(id).delete();
 }
 
-// export const leeme = () => {
-//   const db = firebase.firestore();
-//   db.collection("publicaciones").get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         console.log(doc.data().descripcion);
-
-        // const muestrameTitulo = document.getElementById('tituloPost');
-        // const muestramePublicacion= document.getElementById('post'); 
-        // const informPost= `<h3>${doc.data().titulo} </h3>`
-        // const informPub= `<h3>${doc.data().descripcion}</h3>`
-        // muestrameTitulo.innerHTML += informPost;
-        // muestramePublicacion.innerHTML += informPub;
-  //   });
-  // });
-
-
-// }
-// funcion para agregar datos 
-// const db= firebase.firestore(); 
-// //agregar documentos
-// db.collection("users").add({
-//   first: "Ada",
-//   last: "Lovelace",
-//   born: 1815
-// })
-// .then(function(docRef) {
-//   console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
-//   console.error("Error adding document: ", error);
-// });
+export const editar = (id)=>{
+  const db = firebase.firestore();
+  db.collection("publicaciones").doc(id).update();
+}
--- src/lib/view/templateInicio.js
+++ src/lib/view/templateInicio.js
@@ -1,8 +1,8 @@
-import { cerrarSesionApp, posteame} from '../../firebase.js'
-export const inicio = () =>{
-    const divinicio  =  document.createElement('div');
-    
-    const viewinicio  = `
+import { cerrarSesionApp, posteame, leeme } from "../../firebase.js";
+export const inicio = () => {
+  const divinicio = document.createElement("div");
+
+  const viewinicio = `
     <header>
             <div class="container">
                 <img class="logoD" src='./img/logoDesserts.jpg'>
@@ -20,33 +20,36 @@ export const inicio = () =>{
 
         <!--Contenido Principal-->
 
-<form id= "task-form">
-<div class="form-Titulo">
-<input id="titulo"class="tituloClass" placeholder="Titulo"/>
-</div>
-<div class="form-group">
-<textarea id="task-description" row="4" class="form-control" placehold="Description">
-</textarea>
-</div>
- <button class ="btn-primary" id= "btn-task-form">Publicar</button>
-</form>
-<p id="tituloPost"></p>
-<p id ="post"></p>
-    `;
-    divinicio.innerHTML= viewinicio;
-// informacion para cerrar sesion 
-const cerrarSesion= divinicio.querySelector('#cierre')
-cerrarSesion.addEventListener('click', ()=>{
+ <form id= "task-form">
+    <div class="form-Titulo">
+     <input id="titulo"class="tituloClass" placeholder="Titulo"/>
+       </div>
+        <div class="form-group">
+       <textarea id="task-description" row="4" class="form-control" placehold="Description">
+        </textarea>
+       </div>
+     <button class ="btn-primary" id= "btn-task-form">Publicar</button>
+ </form>
+  <div id='vistaPost'></div>
+ 
+
+        `;
+  divinicio.innerHTML = viewinicio;
+  // informacion para cerrar sesion
+  const cerrarSesion = divinicio.querySelector("#cierre");
+  cerrarSesion.addEventListener("click", () => {
     cerrarSesionApp();
-}); 
-// variables para guardar post 
-const formularioPublic= divinicio.querySelector('#task-form');
-formularioPublic.addEventListener('submit',(event) => {
-event.preventDefault();
-const titulo = divinicio.querySelector('#titulo').value;
-const  descripcion = divinicio.querySelector('#task-description').value;
-posteame({ nuevo: titulo, nueva: descripcion});
-formularioPublic.reset();
-});
-    return divinicio ;
-}
+  });
+  // variables para guardar post
+  const formularioPublic = divinicio.querySelector("#task-form");
+  formularioPublic.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const titulo = divinicio.querySelector("#titulo").value;
+    const descripcion = divinicio.querySelector("#task-description").value;
+
+    posteame({ titulo, descripcion });
+    formularioPublic.reset();
+  });
+  leeme();
+  return divinicio;
+};
